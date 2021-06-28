98804e3de1567842e38a332aea346da57d238dd0
Lance Ellis <scdelance@gmail.com>
2021-06-27 21:14:51 -0500

    added database

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,18 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, Response
 from dotenv import load_dotenv
+from flask.typing import StatusCode
+from werkzeug.sansio.response import Response
+from . import db
 
 load_dotenv()
 app = Flask(__name__)
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+db.init_app(app)
 
 @app.route('/')
 def index():
     return render_template('index.html', title="Lance, Joe, Hamdia", url=os.getenv("URL"))
+@app.route('/health')
+def health():
+    return "", 200
