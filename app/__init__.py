b2f6f2205668e58b5d20c9ea1eacd31557e0b77b
HamdiaA <hamdiaaa258@gmail.com>
2021-07-10 19:08:21 -0400

    week 4 changes and errors fixed

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,40 @@ from flask import Flask, render_template, send_from_directory, Response, request
 from dotenv import load_dotenv
 from flask.typing import StatusCode
 from werkzeug.sansio.response import Response
-#from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
-#from app.db import get_db
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import subprocess
 
-load_dotenv()
+#load_dotenv()
+#app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+#db.init_app(app)
+
 app = Flask(__name__)
-app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
-db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
+    user=os.getenv('POSTGRES_USER'),
+    passwd=os.getenv('POSTGRES_PASSWORD'),
+    host=os.getenv('POSTGRES_HOST'),
+    port=5432,
+    table=os.getenv('POSTGRES_DB'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    username = db.Column(db.String(), primary_key=True)
+    password = db.Column(db.String())
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f"<User {self.username}>"
 
 @app.route('/')
 def index():
@@ -20,6 +45,37 @@ def index():
 @app.route('/health')
 def health():
     return "Works",200
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'GET':
+        if request.args.get('error'):
+            error = request.args['error']
+        else:
+            error = ""
+        #return render_template('register.html', title="Register", error=error, url=os.getenv("URL"))
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif UserModel.query.filter_by(username=username).first() is not None:
+            error = f"User {username} is already registered."
+
+        if error is None:
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
+
+    return render_template('register.html', title="Register", error=error, url=os.getenv("URL"))
 
 @app.route('/login', methods=('GET', 'POST'))
 def login():
@@ -33,19 +89,16 @@ def login():
         else:
             error = ""
         return render_template('login.html', title="Login", error=error, reserveText = reserveText, url=os.getenv("URL"))
-
-    elif request.method == 'POST':
+    
+    if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
         error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+        user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
             error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
+        elif not check_password_hash(user.password, password):
             error = 'Incorrect password.'
 
         if error is None:
@@ -53,42 +106,4 @@ def login():
         else:
             return error, 418
     
-    ## TODO: Return a login page
-    return "Login Page not yet implemented", 501
-
-@app.route('/register', methods=('GET', 'POST'))
-def register():
-    if request.method == 'GET':
-        if request.args.get('error'):
-            error = request.args['error']
-        else:
-            error = ""
-        return render_template('register.html', title="Register", error=error, url=os.getenv("URL"))
-
-    elif request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        db = get_db()
-        error = None
-
-        if not username:
-            error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
-            error = f"User {username} is already registered."
-
-        if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
-            return f"User {username} created successfully"
-        else:
-            return error, 418
-
-    ## TODO: Return a restister page
-    return "Register Page not yet implemented", 501
+    return render_template('login.html', title="Login")
